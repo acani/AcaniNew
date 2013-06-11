5fa0e1e1f1ac15a757f9372f94eae42ca5d96b9c
jlitton <jesse.litton@gmail.com>
2013-06-10 22:24:11 -0400

    Profile: Spec editPictureLabel Syntax Updates

--- AcaniSpecs/AACEditProfileViewControllerSpecs.m
+++ AcaniSpecs/AACEditProfileViewControllerSpecs.m
@@ -80,14 +80,12 @@
     UILabel *editPictureLabel = profilePictureButton.subviews[0];
     STAssertEquals([editPictureLabel class], [UILabel class], nil);
     STAssertEqualObjects(editPictureLabel.backgroundColor, [UIColor colorWithWhite:0 alpha:0.5], nil);
-    STAssertEqualObjects(editPictureLabel.font, [UIFont boldSystemFontOfSize: 10],nil);
+    STAssertEqualObjects(editPictureLabel.font, [UIFont boldSystemFontOfSize:10],nil);
     STAssertEqualObjects(editPictureLabel.text, NSLocalizedString(@"edit", nil), nil);
     STAssertEquals(editPictureLabel.textAlignment, NSTextAlignmentCenter, nil);
     STAssertEqualObjects(editPictureLabel.textColor, [UIColor whiteColor], nil);
     STAssertEquals(editPictureLabel.userInteractionEnabled, NO, nil);
     [profilePictureButton addSubview:editPictureLabel];
-    
-    // TODO: Complete.
 
     UILabel *nameLabel = tableHeaderView.subviews[1];
     STAssertEquals([nameLabel class], [UILabel class], nil);
