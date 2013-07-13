eacef08df5c04b021e9997479bd405545fb641be
jlitton <jesse.litton@gmail.com>
2013-07-13 16:49:11 -0600

    Add ACMessagesViewController.m & .h as well as Adding a base template of ACMessagesViewControllerSpecs.m

---
+++ AcaniSpecs/ACMessagesViewControllerSpecs.m
@@ -0,0 +1,8 @@
+#import <SenTestingKit/SenTestingKit.h>
+#import "ACMessagesViewController.h"
+
+@interface ACMessagesViewController : SenTestCase @end
+
+@implementation ACMessagesViewController
+
+@end
